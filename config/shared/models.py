2222eefa1637e48c248f9fac15661c659b953b5d
0MAVM0 <ediconpts9@gmail.com>
2025-05-22 18:16:01 +0500

    Abstract Model - Base

--- config/shared/models.py
+++ config/shared/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Base(models.Model):
+    id = models.UUIDField(unique=True, default=uuid.uuid4, editable=False, primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
