df45f57348750b6b10bad408e21410bb17131700
0MAVM0 <ediconpts9@gmail.com>
2025-05-29 17:47:50 +0500

    Undid 'admin.py' In 'shared' App

--- config/shared/admin.py
+++ config/shared/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from django.apps import apps
 
-app = apps.get_app_config('shared')
-
-for model_name, model in app.models.items():
-    admin.site.register(model)
+# Register your models here.
