3cd42f7e1b010369df6424b309f72e7388ba62d4
0MAVM0 <ediconpts9@gmail.com>
2025-05-31 18:09:40 +0500

    Correcting Errors

--- config/shared/utilities.py
+++ config/shared/utilities.py
@@ -4,7 +4,7 @@ import re
 EMAIL_REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 PHONE_REGEX = r'^\\+?[1-9][0-9]{7,14}$'
 
-def validation_type(user_input):
+def check_email_or_phone(user_input):
     if (re.fullmatch(EMAIL_REGEX, user_input)):
         data = 'email'
     elif (re.fullmatch(PHONE_REGEX, user_input)):
--- config/user/models.py
+++ config/user/models.py
@@ -7,14 +7,14 @@ from django.core.validators import FileExtensionValidator
 from django.db import models
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from shared.models import BaseModel
+from shared.models import Base
 
 ORDINARY_USER, MANAGER, ADMIN = ('ordinary_user', 'manager', 'admin')
 VIA_EMAIL, VIA_PHONE = ('via_email', 'via_phone')
 NEW, CODE_VERIFIED, DONE, PHOTO_DONE = ('new', 'code_verified', 'done', 'photo_done')
 
 
-class User(AbstractUser, BaseModel):
+class User(AbstractUser, Base):
     USER_ROLES = (
         (ORDINARY_USER, ORDINARY_USER),
         (MANAGER, MANAGER),
@@ -56,7 +56,7 @@ class User(AbstractUser, BaseModel):
 
     def check_username(self):
         if not self.username:
-            temp_username = f'instagram-{uuid.uuid4().__str__().split('-')[-1]}'
+            temp_username = f"instagram-{uuid.uuid4().__str__().split('-')[-1]}"
             while User.objects.filter(username=temp_username):
                 temp_username = f'{temp_username}{random.randint(0,9)}'
             self.username = temp_username
@@ -68,7 +68,7 @@ class User(AbstractUser, BaseModel):
 
     def check_pass(self):
         if not self.password:
-            temp_password = f'password-{uuid.uuid4().__str__().split('-')[-1]}'
+            temp_password = f"password-{uuid.uuid4().__str__().split('-')[-1]}"
             self.password = temp_password
 
     def hashing_password(self):
@@ -96,14 +96,14 @@ PHONE_EXPIRE = 2
 EMAIL_EXPIRE = 5
 
 
-class UserConfirmation(BaseModel):
+class UserConfirmation(Base):
     TYPE_CHOICES = (
         (VIA_PHONE, VIA_PHONE),
         (VIA_EMAIL, VIA_EMAIL)
     )
     code = models.CharField(max_length=4)
     verify_type = models.CharField(max_length=31, choices=TYPE_CHOICES)
-    user = models.ForeignKey('users.User', models.CASCADE, related_name='verify_codes')
+    user = models.ForeignKey(User, models.CASCADE, related_name='verify_codes')
     expiration_time = models.DateTimeField(null=True)
     is_confirmed = models.BooleanField(default=False)
 
