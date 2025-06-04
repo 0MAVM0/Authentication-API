ca30628e4c4865b3e2822620a479d821031d3bdc
0MAVM0 <ediconpts9@gmail.com>
2025-06-04 11:13:35 +0500

    Corrected Mistake In Urls

--- config/user/urls.py
+++ config/user/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('registrate/', CreateAPIView.as_view()),
+    path('registrate/', CreateUserAPIView.as_view()),
     path('verificate/', UserVerificationAPIView.as_view()),
     path('update/', ChangeUserAPIView.as_view()),
 ]
